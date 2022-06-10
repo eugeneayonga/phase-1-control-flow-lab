556aedfb46160fa59dc6312e324b2d3b96efb8aa
Eugene <eugeneayonga@gmail.com>
2022-06-10 21:06:37 +0300

    commit tree 61e5f8515810ab96a5486868d83c08261a832d55

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+// TERNARY CHALLENGE
+function ternaryCheckCity(city) {
+  let result;
+
+  city === "NYC" ? result = "Ok, sounds good." : result = "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
+  }
 }
+
+
+// SWITCH CHALLENGE
+/*
+function switchOnCharmFromTip(tip) {
+function switchOnCharmFromTip(tip) {
+
+let result;
+
+switch (switchOnCharmFromTip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+
+    case "not as generous":
+      result = "Thank you.";
+      break;
+
+    default:
+      console.log("Bye.");
+      break;
+  }
+}
+
+console.log(switchOnCharmFromTip("generous"));
+let result;
+
+switch (switchOnCharmFromTip) {
+    case "generous":
+      result = "Thank you so much.";
+      break;
+
+    case "not as generous":
+      result = "Thank you.";
+      break;
+
+    default:
+      console.log("Bye.");
+      break;
+  }
+}
+
+console.log(switchOnCharmFromTip("generous"));
+*/
